cceae4be629aa3b7b7ce2a82c0c737f86892cf7d
Nils Gey <ich@nilsgey.de>
2013-04-04 12:42:22 +0200

    More setup.py meta data. Also include setup.py instructions in the readme

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from distutils.core import setup
 setup(name = "pynsmclient",
       version = 0.1,
       py_modules=["pynsmclient"],
+      author="Nils Gey",
+      author_email="ich@nilsgey.de",
+      url="https://github.com/nilsgey/pynsmclient",
+      description="Python nsmclient is a convenience wrapper around liblo and NSM-OSC syntax to implement Non Session support easily in your own Python programs.",
+      license="GPLv3+",      
       )
