d1e6e6dcdff31744955c51bbafbc22c4733ec835
Nils Gey <ich@nilsgey.de>
2013-04-04 13:01:28 +0200

    Version is a string

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 setup(name = "pynsmclient",
-      version = 0.1,
+      version = "0.1",
       py_modules=["pynsmclient"],
       author="Nils Gey",
       author_email="ich@nilsgey.de",
       url="https://github.com/nilsgey/pynsmclient",
       description="Python nsmclient is a convenience wrapper around liblo and NSM-OSC syntax to implement Non Session support easily in your own Python programs.",
-      license="GPLv3+",      
+      license="GPLv3+",
       )
